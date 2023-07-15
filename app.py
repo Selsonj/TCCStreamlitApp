5a486eef05b9934499030ca4dbc2a26abdf6a5a5
Selson João <joaoselson@gmail.com>
2023-07-15 03:36:26 -0700

    Update app.py
    Reiniciando o processo.

--- app.py
+++ app.py
@@ -1,31 +1,18 @@
-import io
+import subprocess
+import cv2
 import numpy as np
 import streamlit as st
-from PIL import Image
-import tensorflow as tf
-import tensorflow.lite as tflite
+from tensorflow.keras.models import load_model
 
-model_path = "gs://tcc_covid19/model_architecture1.tflite"
+model_path = "gs://tcc_covid19/model_architecture1.h5"
+model = load_model(model_path)
 
 def preprocess_image(image):
-    image = image.resize((224, 224))
-    image = np.array(image) / 255.0
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = cv2.resize(image, (224, 224))
+    image = image / 255.0
     image = np.expand_dims(image, axis=0)
     return image
-
-def load_model_from_gcs(model_path):
-    bucket_name = "tcc_covid19"
-    blob_name = "model_architecture1.tflite"
-
-    storage_client = storage.Client()
-    bucket = storage_client.bucket(bucket_name)
-    blob = bucket.blob(blob_name)
-
-    blob.download_to_filename("model_architecture1.tflite")
-    model = tflite.Interpreter("model_architecture1.tflite")
-    model.allocate_tensors()
-
-    return model
 
 def main():
     st.title("Classificação de COVID-19 em uma imagem de Raio-X")
@@ -34,18 +21,11 @@ def main():
     uploaded_file = st.file_uploader("Escolha uma imagem de Raio-X", type=["jpg", "jpeg", "png"])
 
     if uploaded_file is not None:
-        image = Image.open(uploaded_file)
+        image = cv2.imdecode(np.fromstring(uploaded_file.read(), np.uint8), cv2.IMREAD_COLOR)
         st.image(image, caption="Imagem carregada", use_column_width=True)
 
         preprocessed_image = preprocess_image(image)
-
-        model = load_model_from_gcs(model_path)
-        input_details = model.get_input_details()
-        output_details = model.get_output_details()
-
-        model.set_tensor(input_details[0]['index'], preprocessed_image)
-        model.invoke()
-        prediction = model.get_tensor(output_details[0]['index'])
+        prediction = model.predict(preprocessed_image)
         class_index = np.argmax(prediction)
         class_label = "COVID-19" if class_index == 0 else "Non-COVID-19"
         confidence = prediction[0][class_index] * 100
